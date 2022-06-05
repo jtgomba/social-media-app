51419b99023df33b673084a2bd9e459b72740274
jtgomba <jgomba@hotmail.co.uk>
2022-06-05 14:30:11 +0900

    changed stuff around

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import "dotenv/config";
 
 import postRoutes from "./routes/posts.js";
 
@@ -13,12 +14,10 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const CONNECTION_URL =
-  "mongodb+srv://jgomba:MPuYkGRWyp7djHfO@cluster0.sicydhz.mongodb.net/?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
