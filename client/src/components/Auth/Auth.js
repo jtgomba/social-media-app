615f14319b2612d74e85385f4350dad4ea5267bf
jtgomba <jgomba@hotmail.co.uk>
2022-06-05 20:56:45 +0900

    handling auth

---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+} from "@material-ui/core";
+import { useDispatch } from "react-redux";
+import { useGoogleLogin } from "@react-oauth/google";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import useStyles from "./styles";
+import Input from "./Input";
+import Icon from "./Icon";
+
+const Auth = () => {
+  const classes = useStyles();
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSignup, setIsSignup] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleShowPassword = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  const handleSubmit = () => {};
+  const handleChange = () => {};
+
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    handleShowPassword(false);
+  };
+
+  const login = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      const result = await axios
+        .get("https://www.googleapis.com/oauth2/v3/userinfo", {
+          headers: { Authorization: `Bearer ${tokenResponse.access_token}` },
+        })
+        .then((res) => res.data);
+      const tokenId = tokenResponse?.access_token;
+
+      try {
+        dispatch({ type: "AUTH", data: { result, tokenId } });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    onError: (errorResponse) => console.log(errorResponse),
+  });
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSignup ? "Sign Up" : "Sign In"}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignup && (
+              <>
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                  type="text"
+                />
+                <Input
+                  name="lastName"
+                  label="Last Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                  type="text"
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email"
+              handleChange={handleChange}
+              autoFocus
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? "text" : "password"}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignup && (
+              <Input
+                name="confirmPassword"
+                label="Repeat Password"
+                handleChange={handleChange}
+                type="password"
+              />
+            )}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}>
+            {isSignup ? "Sign Up" : "Sign In"}
+          </Button>
+          <Button
+            className={classes.googleButton}
+            color="primary"
+            fullWidth
+            onClick={() => login()}
+            startIcon={<Icon />}
+            variant="contained">
+            Google Sign In
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                {isSignup
+                  ? "Already have an account? Sign in"
+                  : "Don't have an account? Sign Up"}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
+};
+
+export default Auth;
---
+++ client/src/reducers/auth.js
@@ -0,0 +1,13 @@
+import { AUTH, LOGOUT } from "../constants/actionTypes";
+
+export default (state = { authData: null }, action) => {
+  switch (action.type) {
+    case AUTH:
+      console.log(action?.data);
+      return state;
+    case LOGOUT:
+      return state;
+    default:
+      return state;
+  }
+};
