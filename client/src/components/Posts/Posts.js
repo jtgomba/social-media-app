73500ebdf02192b49b8b78d6cc603d7d51a4d670
jtgomba <jgomba@hotmail.co.uk>
2022-06-15 21:07:13 +0900

    Update Posts.js

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -17,7 +17,7 @@ const Posts = ({ setCurrentId }) => {
     <CircularProgress />
   ) : (
     <Grid
-      className={classes.container}
+      className={classes.mainContainer}
       container
       alignItems="stretch"
       spacing={3}>
